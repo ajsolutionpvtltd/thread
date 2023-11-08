952f6f5b08f83d11d86d06a257362ef150c9061b
ajsolutionpvtltd <numanaljilani001@gmail.com>
2023-11-08 20:48:19 +0530

    redirect

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,10 +1,16 @@
 import AccountProfile from "@/components/forms/AccountProfile"
+import { fetchUser } from "@/lib/actions/user.action";
 import { currentUser } from "@clerk/nextjs"
+import { redirect } from "next/navigation";
 
 async function page() {
   const user : any = await currentUser()
 
-  const userInfo = {}
+  if (!user) return null; 
+
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
+
   const userData = {
       id : user?.id,
       objectId : userInfo?._id,
